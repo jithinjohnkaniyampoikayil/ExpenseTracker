5f714a41eb288d6d18548c3c8c58ee4942a00177
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2021-02-04 22:50:00 +0000

    fixing unit test

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeaderComponent } from './header.component';
 
@@ -10,7 +12,7 @@ describe('HeaderComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-      imports: [MatMenuModule],
+      imports: [MatMenuModule, HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
   }));
 
